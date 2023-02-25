218f69e3aaa7a9505caf525df9007b3f71c6e3f0
zahid mushtaq <mushtaqzahid888@gmail.com>
2023-02-26 01:44:24 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song/>
+      <Player/>
     </div>
   );
 }
