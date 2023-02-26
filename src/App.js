f6bed6d53dc2b8ff7f480125728a01802700c80b
zahid mushtaq <mushtaqzahid888@gmail.com>
2023-02-26 18:14:29 +0530

    added paly pause and update time property

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import Song from "./components/Song";
 import data from "./util"
 
 
+
 function App() {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
       <Song currentSong={currentSong}/>
-      <Player/>
+      <Player 
+      setIsPlaying={setIsPlaying}
+      isPlaying={isPlaying}
+      currentSong={currentSong}
+
+      />
     </div>
   );
 }
