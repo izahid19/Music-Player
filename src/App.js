f2c9fceb1b32a5ac239fe65e012cfd7e0406bc0f
zahid mushtaq <mushtaqzahid888@gmail.com>
2023-02-26 16:11:53 +0530

    Adding out State

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles/app.scss";
 import Player from "./components/Player";
 import Song from "./components/Song";
+import data from "./util"
 
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song/>
+      <Song currentSong={currentSong}/>
       <Player/>
     </div>
   );
