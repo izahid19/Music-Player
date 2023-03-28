d8c90072b94e99edc028a9ebf60b5243b1d02fd2
zahid mushtaq <mushtaqzahid888@gmail.com>
2023-03-28 18:37:50 +0530

    songs added

--- src/util.js
+++ src/util.js
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
-import { Baller, Challa, nolove, werollin } from './assets/songs/music'
+import { Baller, Challa, nolove, werollin, her, Pagol, TibbeyanDaPutt, BuiltDifferent, CelebrityKiller,GShit, 
+MeandMyGf, RackAndRound, SS295, TheseDays, US, MoosaMashup, Levels, TheLastRide } from './assets/songs/music'
 function chillHop() {
   return [
     {
@@ -11,6 +12,16 @@ function chillHop() {
       color: ['#CD607D', '#c94043'],
       id: uuidv4(),
       active: true,
+    },
+    {
+      name: 'Moosa Mashup (Tribute To Sidhu)',
+      cover:
+        'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-zion/zion/1666937882885-Sidhu-Moosewala-wa_0fc33197.jpg',
+      artist: 'SIdhu Moose Wala',
+      audio: MoosaMashup,
+      color: ['#CD607D', '#c94043'],
+      id: uuidv4(),
+      active: false,
     },
     {
       name: 'Baller',
@@ -41,21 +52,128 @@ function chillHop() {
       active: false,
     },
     {
-      name: 'Pagol',
+      name: 'Pagol (Slowed + Reverbed)',
       cover:
         'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/music/1562531793/srch_abcdigital_CA3SS1932001.jpg',
       artist: 'Deep Jandu Ft Bohemia',
-      audio: 'https://mp3.chillhop.com/serve.php/?mp3=9148',
+      audio: Pagol,
+      color: ['#205950', '#2ab3bf'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Her',
+      cover:
+        'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_orchard/20221116050505_197187463015/1668593505007/resources/197187463015.jpg',
+      artist: 'Shubh',
+      audio: her,
       color: ['#EF8EA9', '#ab417f'],
       id: uuidv4(),
       active: false,
     },
     {
-      name: 'Under the City Stars',
+      name: 'Tibeyan Da Putt',
       cover:
-        'https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg',
-      artist: 'Aso, Middle School, Aviino',
-      audio: 'https://mp3.chillhop.com/serve.php/?mp3=10074',
+        'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_onedigital/music/810059342408/1628765172/srch_onedigital_810059342408_INU251900415.jpg',
+      artist: 'Sidhu Moose Wala',
+      audio: TibbeyanDaPutt,
+      color: ['#CD607D', '#c94043'],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: '295',
+      cover: 'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/810063889609/1628580662/srch_hungama_69279611.jpg',
+      artist: 'Sidhu Moose Wala',
+      audio: SS295,
+      color: ['#205950', '#2ab3bf'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'These Days',
+      cover: 'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/810063889609/1628580662/srch_hungama_69279611.jpg',
+      artist: 'SIdhu Moose Wala ft Bohemia',
+      audio: TheseDays,
+      color: ['#EF8EA9', '#ab417f'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Us',
+      cover:
+        'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/810063889609/1628580662/srch_hungama_69279611.jpg',
+      artist: 'SIdhu Moose Wala ft Raja Kumari',
+      audio: US,
+      color: ['#CD607D', '#c94043'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'G-Shit',
+      cover:
+        'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/810063889609/1628580662/srch_hungama_69279611.jpg',
+      artist: "SIdhu Moose Wala",
+      audio: GShit,
+      color: ['#EF8EA9', '#ab417f'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Celebrity Killer',
+      cover:
+        'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/810063889609/1628580662/srch_hungama_69279611.jpg',
+      artist: 'SIdhu Moose Wala',
+      audio: CelebrityKiller,
+      color: ['#205950', '#2ab3bf'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Built Different',
+      cover:
+        'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/810063889609/1628580662/srch_hungama_69279611.jpg',
+      artist: 'Sidhu Moose Wala',
+      audio: BuiltDifferent,
+      color: ['#CD607D', '#c94043'],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: 'Me and My GirlFriend',
+      cover: 'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/810063889609/1628580662/srch_hungama_69279611.jpg',
+      artist: 'Sidhu Moose Wala',
+      audio: MeandMyGf,
+      color: ['#205950', '#2ab3bf'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Racks and Rounds',
+      cover: 'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/810063889609/1628580662/srch_hungama_69279611.jpg',
+      artist: 'SIdhu Moose Wala ft Sikandar Khalon',
+      audio: RackAndRound,
+      color: ['#EF8EA9', '#ab417f'],
+      id: uuidv4(),
+      active: false,
+    },
+  
+    {
+      name: 'The Last Ride',
+      cover:
+        'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_onedigital/music/update/810105712582/1652793006/srch_onedigital_810105712582_INU252204113.jpg',
+      artist: "SIdhu Moose Wala",
+      audio: TheLastRide,
+      color: ['#EF8EA9', '#ab417f'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Level',
+      cover:
+        'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_onedigital/music/810105713220/1653455406/srch_onedigital_810105713220_INU252204222.jpg',
+      artist: 'SIdhu Moose Wala ft Sunny Malton',
+      audio: Levels,
       color: ['#205950', '#2ab3bf'],
       id: uuidv4(),
       active: false,
